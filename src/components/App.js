6007b9761817417af2d7657a6a48f1139fbf37bf
Bill Hefty <billhefty@gmail.com>
2017-01-06 20:42:28 -0600

    Spacebar pauses/resumes scroll

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     super()
     this.state = {
       isScrolling: false,
-      scrollRate: 100,
+      scrollSpeed: 10,
       promptText: `Prompting begin! Nam bibendum sapien at sapien finibus, molestie dapibus augue suscipit. Quisque finibus velit vel dui pulvinar aliquet. Vivamus ex tortor, ultricies ac mi eu, convallis tristique nulla. Pellentesque id sagittis felis. Integer at porta turpis, eu ornare mi. Ut pellentesque interdum justo eu placerat. Sed lectus augue, sollicitudin vel ante et, elementum fringilla nisi. Quisque tristique lorem dolor, at sodales diam commodo a. Nullam in nisl nec ligula scelerisque facilisis.
 
 Sed vulputate orci sollicitudin metus iaculis facilisis. Fusce ullamcorper metus purus, at ullamcorper nisi lacinia vulputate. Etiam a sapien tristique, rhoncus ex rutrum, commodo tellus. Nam ipsum est, eleifend ac neque eget, interdum tempor augue. Morbi non justo a dolor eleifend aliquam. Aliquam molestie sem vel aliquet molestie. Quisque facilisis ullamcorper odio, vel porta est blandit nec. Cras ac leo sit amet urna porttitor pharetra. Suspendisse commodo interdum justo, vel viverra ligula egestas at. Maecenas et dolor ac lectus cursus varius ac sit amet libero. Nam ac condimentum enim. Nam odio metus, dictum vitae diam at, egestas dignissim nibh. Sed nec molestie sem, quis mattis nunc. In pretium facilisis ex, ac dictum nulla pellentesque a.
@@ -19,6 +19,13 @@ Nunc in scelerisque metus. Nunc ultricies, mi laoreet malesuada tempus, velit qu
     this.scrollInterval = () => undefined
     this.handleFlip = this.handleFlip.bind(this)
     this.scrollPrompter = this.scrollPrompter.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  componentDidMount() {
+    document.getElementById('prompt-container').addEventListener('keydown', (e) => {
+      this.handleKeyPress(e)
+    }, false)
   }
 
   handleFlip() {
@@ -37,18 +44,40 @@ Nunc in scelerisque metus. Nunc ultricies, mi laoreet malesuada tempus, velit qu
     if (!this.state.isScrolling) {
       this.setState({isScrolling: true})
       this.scrollInterval = setInterval(() => {
-        $('#prompt-container').animate({ scrollTop: speed }, this.state.scrollRate, 'linear')
-        if (direction === 'forward') speed += 500
-        if (direction === 'backward') speed -= 500
+        $('#prompt-container').animate({ scrollTop: speed }, 100, 'linear')
+        if (direction === 'forward') speed += this.state.scrollSpeed
+        if (direction === 'backward') speed -= this.state.scrollSpeed
         if (
             ((promptDiv.scrollTop >= (promptDiv.scrollHeight - promptDiv.offsetHeight)) && direction === 'forward') ||
             ((promptDiv.scrollTop === 0) && direction === 'backward')
           ) {
             this.pauseScroll()
         }
-      }, this.state.scrollRate)
+      }, 100)
     } else {
       this.pauseScroll()
+    }
+  }
+
+  handleKeyPress(e) {
+    e.preventDefault()
+    switch (e.keyCode) {
+      case 38:
+        console.log('up')
+        break
+      case 40:
+        console.log('down')
+        break
+      case 32:
+        if (this.state.isScrolling) {
+          this.pauseScroll()
+        } else {
+          this.scrollPrompter('forward')
+        }
+        break
+      default:
+        console.log('nope')
+        break
     }
   }
 
@@ -57,19 +86,14 @@ Nunc in scelerisque metus. Nunc ultricies, mi laoreet malesuada tempus, velit qu
       <div className='prompt-parent'>
         <Navigation />
 
-        <div id='prompt-container' className='prompt-container noselect'
+        <textarea readOnly id='prompt-container' className='prompt-container noselect'
           onClick={() => {
             this.scrollPrompter('forward')
           }}
           onContextMenu={(e) => {
             e.preventDefault()
             this.scrollPrompter('backward')
-          }}>
-            ---BEGIN---
-            {this.state.promptText}
-            ---END---
-
-        </div>
+          }} value={this.state.promptText}/>
 
         <div>
           <Button
