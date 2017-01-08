fb8a7793e93390c8e856f3ff69e9f80aec64d04d
Bill Hefty <billhefty@gmail.com>
2017-01-07 18:01:05 -0600

    Add placeholder boilerplate for Auth0

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Router, Route } from 'react-router'
+import { Router, Route, IndexRedirect } from 'react-router'
+import AuthService from './utils/AuthService'
 
-import App from './components/App'
+// import App from './components/App'
+import Container from './Container'
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+
+const auth = new AuthService('vTXrv6wu2uoMG7pWIYJlfn5XwAkeKDFE', 'bhefty.auth0.com')
+
+// validate authentication for private routes
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' })
+  }
+}
 
 const Routes = (props) => (
   <Router {...props}>
-    <Route path='/' component={App} />
+    <Route path='/' component={Container} auth={auth}>
+      <IndexRedirect to='/home' />
+      <Route path='home' component={Home} onEnter={requireAuth} />
+      <Route path='login' component={Login} />
+    </Route>
   </Router>
 )
 
