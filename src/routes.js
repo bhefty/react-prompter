163c823da05908443e1ce68f2f688648a6842dd3
Bill Hefty <billhefty@gmail.com>
2017-01-08 14:18:13 -0600

    Add proxy for development server

--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,34 @@ const requireAuth = (nextState, replace) => {
   }
 }
 
+function search(query, cb) {
+  return fetch('scripts', {
+    accept: 'application/json',
+  }).then(checkStatus)
+    .then(parseJSON)
+    .then(cb)
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    const error = new Error(`HTTP Error ${response.statusText}`)
+    error.status = response.statusText
+    error.response = response
+    console.log(error)
+    throw error
+  }
+}
+
+function parseJSON(response) {
+  return response.json()
+}
+
+search('all', (result) => {
+  console.log(result)
+})
+
 const Routes = (props) => (
   <Router {...props}>
     <Route path='/' component={Container} auth={auth}>
