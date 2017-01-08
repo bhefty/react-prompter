2db96327a13415bb4d7ab1f2ec5ca9ea3155482f
Bill Hefty <billhefty@gmail.com>
2017-01-08 11:42:46 -0600

    Add MONGO_URI for DB connection

--- server/app.js
+++ server/app.js
@@ -6,7 +6,15 @@ const getClientEnvironment = require('../config/env')
 
 const app = express()
 
-// mongoose.connect()
+const MONGO_URI = process.env.REACT_APP_MONGO_URI || 'mongodb://localhost/react-prompter-dev'
+mongoose.connect(MONGO_URI)
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('connected!')
+})
+
 let env = getClientEnvironment('')
 console.log('process.env', env)
 
