4c67096eb075ba49375de274b7b475754a545d42
Bill Hefty <billhefty@gmail.com>
2017-01-08 20:28:16 -0600

    Add demo mode

---
+++ src/components/Login.js
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes as T } from 'react'
+import { Button } from 'react-bootstrap'
+import AuthService from '../utils/AuthService'
+
+class Login extends Component {
+  static contextTypes = {
+    router: T.object
+  }
+
+  static propTypes = {
+    location: T.object,
+    auth: T.instanceOf(AuthService)
+  }
+
+  constructor(props, context) {
+    super(props, context)
+    props.auth.on('profile_updated', (newProfile) => {
+      this.setState({ profile: newProfile })
+    })
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+
+  handleLogin() {
+    this.props.auth.login()
+  }
+
+  render() {
+    return(
+      <div className='Login'>
+        <h1>Choose an option:</h1>
+          <div className='well'>
+          <Button bsStyle='primary'
+            bsSize='large'
+            block
+            onClick={() => this.context.router.push('/demo')}>
+            Quick Start - Demo
+          </Button>
+          <Button bsStyle='success'
+            bsSize='large'
+            block
+            onClick={this.handleLogin}>
+            Login
+          </Button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,8 @@ import AuthService from './utils/AuthService'
 import App from './components/App'
 import Container from './Container'
 import NotFound from './components/NotFound'
-import Login from './components/Login/Login'
+import Login from './components/Login'
+import Demo from './components/Demo'
 
 const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID
 const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN
@@ -18,40 +19,13 @@ const requireAuth = (nextState, replace) => {
   }
 }
 
-function search(query, cb) {
-  return fetch('scripts', {
-    accept: 'application/json',
-  }).then(checkStatus)
-    .then(parseJSON)
-    .then(cb)
-}
-
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response
-  } else {
-    const error = new Error(`HTTP Error ${response.statusText}`)
-    error.status = response.statusText
-    error.response = response
-    console.log(error)
-    throw error
-  }
-}
-
-function parseJSON(response) {
-  return response.json()
-}
-
-search('all', (result) => {
-  // console.log(result)
-})
-
 const Routes = (props) => (
   <Router {...props}>
     <Route path='/' component={Container} auth={auth}>
       <IndexRedirect to='/home' />
       <Route path='home' component={App} onEnter={requireAuth} />
       <Route path='login' component={Login} />
+      <Route path='demo' component={Demo} />
       <Route path='*' component={NotFound} />
     </Route>
   </Router>
