ae95438c2d8ad506981f8df8bbe85d5a673792df
Bill Hefty <billhefty@gmail.com>
2017-01-14 10:48:58 -0600

    Refactor to presentational component

--- src/components/PrompterControls.js
+++ src/components/PrompterControls.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Button } from 'react-bootstrap'
 
-class PrompterControls extends Component {
-  render() {
-    return (
-      <div className='PrompterControls'>
-          <Button
-            bsStyle="primary"
-            onClick={() => this.props.onFlip()}>
-              Mirror Text
-          </Button>
-          <Button onClick={() => {
-            this.props.onScrollPrompter('forward')
-          }}>Scroll</Button>
+export default function PrompterControls(props) {
+  let { onFlip, onScrollPrompter } = props
+  return (
+    <div className='PrompterControls'>
+        <Button
+          bsStyle="primary"
+          onClick={() => onFlip()}>
+            Mirror Text
+        </Button>
+        <Button onClick={() => {
+          onScrollPrompter('forward')
+        }}>Scroll</Button>
 
-          <Button onClick={() => {
-            this.props.onScrollPrompter('backward')
-          }}>Reverse</Button>
-      </div>
-    )
-  }
+        <Button onClick={() => {
+          onScrollPrompter('backward')
+        }}>Reverse</Button>
+    </div>
+  )
 }
-
-export default PrompterControls
